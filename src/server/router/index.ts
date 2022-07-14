cd8dcf198e1cae54473615df6a74fa2eb452ab42
Raghav Tiruvallur <rtiruvallur@gmail.com>
2022-07-14 20:18:57 +0530

    Add placement router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,14 +1,16 @@
 // src/server/router/index.ts
-import { createRouter } from "./context";
-import superjson from "superjson";
+import { createRouter } from './context';
+import superjson from 'superjson';
 
-import { exampleRouter } from "./example";
-import { authRouter } from "./auth";
+import { exampleRouter } from './example';
+import { authRouter } from './auth';
+import { placementRouter } from './placement-event';
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("auth.", authRouter);
+  .merge('example.', exampleRouter)
+  .merge('auth.', authRouter)
+  .merge('placement.', placementRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
