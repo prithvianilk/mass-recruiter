259a9e1e65b375980ebdf5acdcdc22f14a5684cc
prithvianilk <thepirateking3d2y@gmail.com>
2022-07-14 20:06:20 +0530

    Add NextAuth & format
    - Added basic google auth via NextAuth
    - Made code pretty

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,35 +1,18 @@
-import NextAuth, { type NextAuthOptions } from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
-import CredentialsProvider from 'next-auth/providers/credentials'
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
 // Prisma adapter for NextAuth, optional and can be removed
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { prisma } from '../../../server/db/client'
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from '../../../server/db/client';
 
 export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-    // ...add more providers here
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        name: {
-          label: 'Name',
-          type: 'text',
-          placeholder: 'Enter your name',
-        },
-      },
-      async authorize(credentials, _req) {
-        const user = { id: 1, name: credentials?.name ?? 'J Smith' }
-        return user
-      },
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
